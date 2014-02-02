80b37c78e979dfd438ca7155aeff12e816c0d74a
Morten Hjorth Fæster <faester@gmail.com>
2014-02-02 22:07:51 +0100

    Some defaults.

--- src/main/java/dk/mfaester/grav/Equilibrator.java
+++ src/main/java/dk/mfaester/grav/Equilibrator.java
@@ -3,6 +3,12 @@ package dk.mfaester.grav;
  * Created by Morten.Faester on 09-01-14.
  */
 public class Equilibrator {
-
+    /*
+    100 1   -1  -1  0   1
+    100 -1  1   0   0   1
+    200 -1  0   1   0   1
+    100 0   0   0   1   1
+    500 1   1   1   1   1
+     */
 }
 
